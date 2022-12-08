87483a7ca532bb7af658592d8345c990772fd040
PierreYEM <pierreyem@orange.fr>
2022-12-08 01:51:11 +0100

    maj

--- ECF-blanc-pk/pkm.js
+++ ECF-blanc-pk/pkm.js
@@ -1,39 +1,34 @@
 const api = 'https://pokeapi.co/api/v2/pokemon/';
-let tabImage = [...document.querySelectorAll('.pkmimg')],
-    tabName = [...document.querySelectorAll('.name')],
-    tabPokemon = [...document.querySelectorAll('.pokemon')],
-    tabNumber = [...document.querySelectorAll('.number')],
-    tabModalImg = [...document.querySelectorAll('#modalImg')],
-
-
-
-
+let bloc = document.querySelector('.bloc'),
     modal = document.querySelector('.modal'),
     modalPkm = document.querySelector('.modalPkm'),
     modalImg = document.querySelector('#modalImg'),
     arrow = document.querySelector('.arrow'),
-    modalNb = document.querySelector('.modalNb');
+    modalNb = document.querySelector('.modalNb'),
+    modalDetail = document.querySelector('.detail');
 
 
-
-for (let i = 0; i < tabImage.length; i++) {
+for (let i = 0; i < 151; i++) {
 
     fetch(api + [i + 1] + '/')
-        .then(response => response.json())
+        .then(data => data.json())
         .then(json => {
+            createBloc();
             console.log(json);
+            let tabImage = [...document.querySelectorAll('.pkmimg')],
+                tabName = [...document.querySelectorAll('.name')],
+                tabPokemon = [...document.querySelectorAll('.pokemon')],
+                tabNumber = [...document.querySelectorAll('.number')];
+
             tabImage[i].style.backgroundImage = "url(" + json.sprites.front_default + ")";
-
             tabName[i].innerHTML = json.name;
-
             tabNumber[i].innerHTML = `#${json.id.toString().padStart(3, 0)}`;
 
             // déclencheur au click
 
             tabPokemon[i].addEventListener('click', () => {
 
-                modalImg.innerHTML = '<img src="' + json.sprites.front_default + '">'
-
+                modalDetail.innerHTML = '<img src="' + json.sprites.other["official-artwork"].front_default + '">'
 
                 modalPkm.innerHTML = json.name;
 
@@ -49,4 +44,21 @@ for (let i = 0; i < tabImage.length; i++) {
 
 
         })
+}
+function createBloc() {
+    let pokemon = document.createElement('div'),
+        pkmimg = document.createElement('div'),
+        name = document.createElement('div'),
+        number = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    pkmimg.classList.add('pkmimg');
+    name.classList.add('name');
+    number.classList.add('number');
+    bloc.appendChild(pokemon);
+    pkmimg.appendChild(number);
+    pokemon.appendChild(pkmimg);
+    pokemon.appendChild(name);
+}
+function createModal() {
+
 }
